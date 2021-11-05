5f260ed6441eba954c56cfd27a61a26a9cc6dd76
byteThePizza <faisal.abuomar@outlook.com>
2021-11-05 23:25:18 +0400

    commit1

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import "./topbar.css"
+import {Chat, Notifications, Person, Search} from "@mui/icons-material"
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+           <div className="topbarLeft">
+               <span className="logo">SocialApp</span>
+           </div>
+           <div className="topbarCenter">
+               <div className="searchBar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="search for a friend, post or a video" className="searchInput" />
+               </div>
+           </div>
+           <div className="topbarRight">
+               <div className="topbarLinks">
+                <span className="topbarLink">HomePage</span>
+                <span className="topbarLink">Timeline</span>
+               </div>
+               <div className="topbarIcons">
+                   <div className="topbarIconItem"><Person/>
+                   <span className="itemIconBadge">1</span>
+                   </div>
+                   <div className="topbarIconItem"><Chat/>
+                   <span className="itemIconBadge">2</span>
+                   </div>
+                   <div className="topbarIconItem"><Notifications/>
+                   <span className="itemIconBadge">1</span>
+                   </div>
+               </div>
+               <img src="/assets/1.jpg" alt="" className="profilePicture" />
+           </div>
+        </div>
+    )
+}
